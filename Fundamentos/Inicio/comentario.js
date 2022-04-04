538fc1f6d82849ea3d99bd37eff1e5328604017f
Rodrigo Carvalho <rodrigo.racarvalho@gmail.com>
2022-04-04 20:32:53 -0300

    Primeiro commit para iniciar projeto

---
+++ Fundamentos/Inicio/comentario.js
@@ -0,0 +1,23 @@
+//teste de comentário
+console.log('Passo #01'); //comentário de linha
+console.log('Passo #02')
+// console.log('Passo #03')
+
+/*
+comentário de 
+multiplas
+linhas
+*/
+
+/*
+* tambem
+* comentário de 
+* multiplas 
+* linhas
+* também pode
+* ser escrito
+* com asterisco no inicio
+* de cada linha
+*/
+
+console.log('Passo #03')
---
+++ Fundamentos/Inicio/dados_2.js
@@ -0,0 +1,16 @@
+let preco = 19.9
+let desconto =0.4
+
+console.log(preco*(1-desconto))
+
+let precoComDesconto = preco*(1-desconto)
+
+console.log(precoComDesconto)
+
+let produto = "Caderno"
+let categoria = "Papelaria"
+
+console.log('Produto : '+ produto 
+    +', Categoria : ' + categoria
+    +', Preço : ' + preco
+    +', Desconto : '+ desconto)
