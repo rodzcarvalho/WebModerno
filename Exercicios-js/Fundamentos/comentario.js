07dcf20ff1ae0f035d79eb5ee6e3702bd97311a5
Rodrigo Carvalho <rodrigo.racarvalho@gmail.com>
2022-04-08 23:19:08 -0300

    Fundamentos

---
+++ Exercicios-js/Fundamentos/comentario.js
@@ -0,0 +1,18 @@
+//Comentário de uma linha
+console.log('Linha 1') // pode ser depois tb
+
+/*
+comentário
+de
+multiplas
+linhas
+*/
+console.log('Linha 2')
+
+/*
+* comentario
+* de multiplas
+* linhas
+* pode ser assim tb
+* com asterisco na frente.
+*/
---
+++ Exercicios-js/Fundamentos/organizacao.js
@@ -0,0 +1,10 @@
+//O Javascript é organizao em Sentenças e blocos
+console.log("Sentença de código")
+
+//Bloco porde haver bloco dento de bloco.
+{
+    {
+        console.log("Olá")
+        console.log('Mundo!')
+    }
+}
